5fd6949532ecdb053fff94188e2706eed78782e1
TomCube <121310847+yomcube@users.noreply.github.com>
2025-03-24 22:31:51 -0500

    Add offsets to savefile.h

--- include/penguin/savefile.h
+++ include/penguin/savefile.h
@@ -18,52 +18,52 @@ struct SaveData {
         */
 
         //u8 version[2];						// version and subversion; always 0xE and 0x0.
-        u16 version;
-        u8 lastSelectedFile;				// 0 to 2.
-        u8 _7;								// unused. speculated to be padding.
-        u16 freeModePlayCount[WORLD_COUNT][STAGE_COUNT];
-        u16 coinBattlePlayCount[WORLD_COUNT][STAGE_COUNT];
-        u16 extraModesUnlockedWorlds;		// worlds unlocked in extra modes.
-        u16 _69A;							// unused. speculated to be padding.
-        u32 crc32;							// calculated checksum (excluding the magic)
+        /*0x000*/ u16 version;
+        /*0x002*/ u8 lastSelectedFile;				// 0 to 2.
+        /*0x003*/ u8 _7;								// unused. speculated to be padding.
+        /*0x004*/ u16 freeModePlayCount[WORLD_COUNT][STAGE_COUNT];
+        /*0x34C*/ u16 coinBattlePlayCount[WORLD_COUNT][STAGE_COUNT];
+        /*0x694*/ u16 extraModesUnlockedWorlds;		// worlds unlocked in extra modes.
+        /*0x69A*/ u16 _69A;							// unused. speculated to be padding.
+        /*0x69C*/ u32 crc32;							// calculated checksum (excluding the magic)
     };
 
     static_assert(sizeof(SaveData::Header) == 0x6A0, "Header size mismatch!"); // wrong size
 
     struct SaveSlot {
         //u8 version[2];									// version and subversion; always 0xE and 0x0.
-        u16 version;
-        u8 gameCompletion;
-        u8 currentWorld;
-        u8 currentSubworld;
-        u8 currentPathNode;
-        u8 w5VineReshuffleCounter;						/* used in World 5. this value decreases when a
+        /*0x000*/ u16 version;
+        /*0x002*/ u8 gameCompletion;
+        /*0x003*/ u8 currentWorld;
+        /*0x004*/ u8 currentSubworld;
+        /*0x005*/ u8 currentPathNode;
+        /*0x006*/ u8 w5VineReshuffleCounter;						/* used in World 5. this value decreases when a
                                                         level is played. upon reaching zero, the vines will reshuffle. */
-        bool w3SwitchOn;								// used in World 3. indicates the red switch's status.
-        u8 _8;											// unused.
-        u8 stockItemCount[POWERUP_COUNT];
-        u8 startingMushroomHouseType[WORLD_COUNT];
-        u8 playerContinues[PLAYER_COUNT];
-        u8 playerCoins[PLAYER_COUNT];
-        u8 playerLives[PLAYER_COUNT];
-        u8 playerSpawnFlags[PLAYER_COUNT];
-        u8 playerCharacter[PLAYER_COUNT];
-        u8 playerPowerup[PLAYER_COUNT];
-        u8 worldCompletion[WORLD_COUNT];
-        u8 enemyRevivalCount[WORLD_COUNT][AMBUSH_ENEMY_COUNT];
-        u16 _64;										// unused.
-        u16	staffCreditsHighScore;
-        u32 score;										// regular score. yknow, the one you see in levels.
-        u32 stageCompletion[WORLD_COUNT][STAGE_COUNT];	// level completion flags.
-        bool hintMovieBought[HINT_MOVIE_COUNT];			// indicates whether or not a Hint Movie has been bought.
-        u8 toadRescueLevel[WORLD_COUNT];				// Toad rescue level for each world.
-        u8 enemySubworldNumber[WORLD_COUNT][AMBUSH_ENEMY_COUNT];	// subworld number for each map enemy.
-        u8 enemyPosIndex[WORLD_COUNT][AMBUSH_ENEMY_COUNT]; // path node for each map enemy.
-        u8 enemyWalkDirection[WORLD_COUNT][AMBUSH_ENEMY_COUNT];
-        u8 deathCount[WORLD_COUNT][STAGE_COUNT];		// death count for each level.
-        u8 deathCountW3Level4Switch;					// death count for the "red switch active" variant of World 3-4.
-        u8 padding[0x13];
-        u32 crc32;										// checksum.
+        /*0x007*/ bool w3SwitchOn;								// used in World 3. indicates the red switch's status.
+        /*0x008*/ u8 _8;											// unused.
+        /*0x009*/ u8 stockItemCount[POWERUP_COUNT];
+        /*0x010*/ u8 startingMushroomHouseType[WORLD_COUNT];
+        /*0x01A*/ u8 playerContinues[PLAYER_COUNT];
+        /*0x01E*/ u8 playerCoins[PLAYER_COUNT];
+        /*0x022*/ u8 playerLives[PLAYER_COUNT];
+        /*0x026*/ u8 playerSpawnFlags[PLAYER_COUNT];
+        /*0x02A*/ u8 playerCharacter[PLAYER_COUNT];
+        /*0x02E*/ u8 playerPowerup[PLAYER_COUNT];
+        /*0x032*/ u8 worldCompletion[WORLD_COUNT];
+        /*0x03C*/ u8 enemyRevivalCount[WORLD_COUNT][AMBUSH_ENEMY_COUNT];
+        /*0x064*/ u16 _64;										// unused.
+        /*0x066*/ u16	staffCreditsHighScore;
+        /*0x068*/ u32 score;										// regular score. yknow, the one you see in levels.
+        /*0x06C*/ u32 stageCompletion[WORLD_COUNT][STAGE_COUNT];	// level completion flags.
+        /*0x6FC*/ bool hintMovieBought[HINT_MOVIE_COUNT];			// indicates whether or not a Hint Movie has been bought.
+        /*0x742*/ u8 toadRescueLevel[WORLD_COUNT];				// Toad rescue level for each world.
+        /*0x74C*/ u8 enemySubworldNumber[WORLD_COUNT][AMBUSH_ENEMY_COUNT];	// subworld number for each map enemy.
+        /*0x774*/ u8 enemyPosIndex[WORLD_COUNT][AMBUSH_ENEMY_COUNT]; // path node for each map enemy.
+        /*0x79C*/ u8 enemyWalkDirection[WORLD_COUNT][AMBUSH_ENEMY_COUNT];
+        /*0x7C4*/ u8 deathCount[WORLD_COUNT][STAGE_COUNT];		// death count for each level.
+        /*0x968*/ u8 deathCountW3Level4Switch;					// death count for the "red switch active" variant of World 3-4.
+        /*0x969*/ u8 padding[0x13];
+        /*0x97C*/ u32 crc32;										// checksum.
     };
 
     static_assert(sizeof(SaveData::SaveSlot) == 0x980, "Save Slot size mismatch!");
